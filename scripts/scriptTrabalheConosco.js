0a639d62cca74c7d3ca73e77944733e8069d0bfe
MateusP-Wolf <mateuspedriniwolf@gmail.com>
2024-02-01 17:27:42 -0300

    testes finais tela de trabalhe conosco

--- scripts/scriptTrabalheConosco.js
+++ scripts/scriptTrabalheConosco.js
@@ -1,4 +1,12 @@
 function enviarFormulario() {
-    alert("Formulário Enviado")
-    
+    event.preventDefault();
+    Swal.fire("Seu formulário foi enviado! Entraremos em contato em breve! (Ou não)");
+
+    document.getElementById("inputEmail4").value = "";
+    document.getElementById("inputPassword4").value = "";
+    document.getElementById("inputPais").value = "";
+    document.getElementById("inputAddress").value = "";
+    document.getElementById("inputCity").value = "";
+    document.getElementById("inputTelefone").value = "";
+    document.getElementById("inputNasc").value = "";
 }
