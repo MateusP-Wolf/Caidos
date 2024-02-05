f7ece51e4bd4d4ef460af001e67680c4df61119e
MateusP-Wolf <mateuspedriniwolf@gmail.com>
2024-02-05 16:19:57 -0300

    pagina de Nos Contrate

---
+++ scripts/scriptNosContrate.js
@@ -0,0 +1,7 @@
+function adicionarClasse(elemento) {
+    elemento.classList.add("animate__pulse");
+  }
+
+  function removerClasse(elemento) {
+    elemento.classList.remove("animate__pulse");
+  }
